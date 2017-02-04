0cee9d334acad055bcbe450c6aaf2fb6b3ec7826
Sam Weiller <sam.weiller@gmail.com>
2017-02-04 17:34:58 -0500

    fixed config vars

--- index.js
+++ index.js
@@ -232,25 +232,17 @@ app.use(express.static('public'));
  */
 
 // App Secret can be retrieved from the App Dashboard
-const APP_SECRET = (process.env.MESSENGER_APP_SECRET) ?
-  process.env.MESSENGER_APP_SECRET :
-  config.get('APP_SECRET');
+const APP_SECRET = (process.env.MESSENGER_APP_SECRET)
 
 // Arbitrary value used to validate a webhook
-const VALIDATION_TOKEN = (process.env.MESSENGER_VALIDATION_TOKEN) ?
-  (process.env.MESSENGER_VALIDATION_TOKEN) :
-  config.get('VALIDATION_TOKEN');
+const VALIDATION_TOKEN = (process.env.MESSENGER_VALIDATION_TOKEN)
 
 // Generate a page access token for your page from the App Dashboard
-const PAGE_ACCESS_TOKEN = (process.env.MESSENGER_PAGE_ACCESS_TOKEN) ?
-  (process.env.MESSENGER_PAGE_ACCESS_TOKEN) :
-  config.get('PAGE_ACCESS_TOKEN');
+const PAGE_ACCESS_TOKEN = (process.env.MESSENGER_PAGE_ACCESS_TOKEN)
 
 // URL where the app is running (include protocol). Used to point to scripts and
 // assets located at this address.
-const SERVER_URL = (process.env.SERVER_URL) ?
-  (process.env.SERVER_URL) :
-  config.get('SERVER_URL');
+const SERVER_URL = (process.env.SERVER_URL)
 
 if (!(APP_SECRET && VALIDATION_TOKEN && PAGE_ACCESS_TOKEN && SERVER_URL)) {
   console.error("Missing config values");
