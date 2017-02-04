08ec500f27addd1562d595ff233a75dcd1df5f6f
Sam Weiller <sam.weiller@gmail.com>
2017-02-04 16:46:29 -0500

    changed webhook

--- index.js
+++ index.js
@@ -19,8 +19,8 @@ app.get('/', function (req, res) {
 })
 
 // for Facebook verification
-app.get('/webhook/', function (req, res) {
-    if (req.query['hub.verify_token'] === 'my_voice_is_my_password_verify_me') {
+app.get('/webhook', function (req, res) {
+    if (req.query['hub.verify_token'] === 'my_password_is_my_voice_blah_verify_me') {
         res.send(req.query['hub.challenge'])
     }
     res.send('Error, wrong token')
