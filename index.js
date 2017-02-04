5d2b6ec3d23b6dc7111dc07e13e27c30c6599971
Sam Weiller <sam.weiller@gmail.com>
2017-02-04 16:59:21 -0500

    new section

--- index.js
+++ index.js
@@ -31,36 +31,37 @@ app.listen(app.get('port'), function() {
     console.log('running on port', app.get('port'))
 })
 
-// app.post('/webhook/', function (req, res) {
-//     let messaging_events = req.body.entry[0].messaging
-//     for (let i = 0; i < messaging_events.length; i++) {
-//         let event = req.body.entry[0].messaging[i]
-//         let sender = event.sender.id
-//         if (event.message && event.message.text) {
-//             let text = event.message.text
-//             sendTextMessage(sender, "Text received, echo: " + text.substring(0, 200))
-//         }
-//     }
-//     res.sendStatus(200)
-// })
+app.post('/webhook', function (req, res) {
+  var data = req.body;
 
-// const token = FB_PAGE_ACCESS_TOKEN
+  // Make sure this is a page subscription
+  if (data.object === 'page') {
 
-function sendTextMessage(sender, text) {
-    let messageData = { text:text }
-    request({
-        url: 'https://graph.facebook.com/v2.6/me/messages',
-        qs: {access_token:token},
-        method: 'POST',
-        json: {
-            recipient: {id:sender},
-            message: messageData,
+    // Iterate over each entry - there may be multiple if batched
+    data.entry.forEach(function(entry) {
+      var pageID = entry.id;
+      var timeOfEvent = entry.time;
+
+      // Iterate over each messaging event
+      entry.messaging.forEach(function(event) {
+        if (event.message) {
+          receivedMessage(event);
+        } else {
+          console.log("Webhook received unknown event: ", event);
         }
-    }, function(error, response, body) {
-        if (error) {
-            console.log('Error sending messages: ', error)
-        } else if (response.body.error) {
-            console.log('Error: ', response.body.error)
-        }
-    })
+      });
+    });
+
+    // Assume all went well.
+    //
+    // You must send back a 200, within 20 seconds, to let us know
+    // you've successfully received the callback. Otherwise, the request
+    // will time out and we will keep trying to resend.
+    res.sendStatus(200);
+  }
+});
+
+function receivedMessage(event) {
+  // Putting a stub for now, we'll expand it in the following steps
+  console.log("Message data: ", event.message);
 }
