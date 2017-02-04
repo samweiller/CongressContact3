78f1a09780e39e136e8dc7c9dcff2e7dff7b5e64
Sam Weiller <sam.weiller@gmail.com>
2017-02-04 16:52:45 -0500

    new section

--- index.js
+++ index.js
@@ -19,14 +19,29 @@ app.get('/', function (req, res) {
 })
 
 // for Facebook verification
-app.get('/webhook', function (req, res) {
-    if (req.query['hub.verify_token'] === 'my_password_is_my_voice_blah_verify_me') {
-        res.send(req.query['hub.challenge'])
-    }
-    res.send('Error, wrong token')
-})
+// app.get('/webhook', function (req, res) {
+//     if (req.query['hub.verify_token'] === 'my_password_is_my_voice_blah_verify_me') {
+//         res.send(req.query['hub.challenge'])
+//     }
+//     res.send('Error, wrong token')
+// })
 
 // Spin up the server
 app.listen(app.get('port'), function() {
     console.log('running on port', app.get('port'))
 })
+
+app.post('/webhook/', function (req, res) {
+    let messaging_events = req.body.entry[0].messaging
+    for (let i = 0; i < messaging_events.length; i++) {
+        let event = req.body.entry[0].messaging[i]
+        let sender = event.sender.id
+        if (event.message && event.message.text) {
+            let text = event.message.text
+            sendTextMessage(sender, "Text received, echo: " + text.substring(0, 200))
+        }
+    }
+    res.sendStatus(200)
+})
+
+const token = FB_PAGE_ACCESS_TOKEN
