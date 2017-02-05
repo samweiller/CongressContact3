5e6e66a3f10bffe3902181b025c654d05b104b85
Sam Weiller <sam.weiller@gmail.com>
2017-02-04 22:01:18 -0500

    more testing

--- index.js
+++ index.js
@@ -379,7 +379,7 @@ function receivedMessage(event) {
                 console.log('Upload successful!  Server responded with:', body);
                 console.log('LOOK HERE')
                 var fooBar = JSON.parse(body);
-                console.log(fooBar)
+                console.log(fooBar.results[0].last_name)
                 sendTextMessage(senderID, "Got it!")
 
                 // CALL SEND API
