040443c0ce132278025b9c3747b176d0f1440616
Sam Weiller <sam.weiller@gmail.com>
2017-02-05 12:32:54 -0500

    bugs

--- index.js
+++ index.js
@@ -538,7 +538,7 @@ function receivedPostback(event) {
       }, 1000)
    } else if (payload.indexOf('GENERATE_SCRIPT') > -1) {
       repIndex = payload[payload.length - 1];
-      console.log(masterRepData.repIndex])
+      console.log(masterRepData[repIndex])
    }
 }
 
