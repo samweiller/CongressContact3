71dc2d52f738d1f3774feb37ee51863de6109755
Sam Weiller <sam.weiller@gmail.com>
2017-02-04 21:22:31 -0500

    fixed brackets

--- index.js
+++ index.js
@@ -391,10 +391,7 @@ function receivedMessage(event) {
                                         "title": "Start Chatting",
                                         "payload": "DEVELOPER_DEFINED_PAYLOAD"
                                     }]
-                                }]
                             }, {
-                                "template_type": "generic",
-                                "elements": [{
                                     "title": "Welcome to Peter\'s Hats",
                                     "image_url": "https://petersfancybrownhats.com/company_image.png",
                                     "subtitle": "We\'ve got the right hat for everyone.",
